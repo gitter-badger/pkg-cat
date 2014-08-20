91c71b23046f6fa43d1401f8f663d09eea18ba37
George Mayorga <georgetmayorga@gmail.com>
2014-08-20 11:43:26 -0400

    Connect Amazon S3 for saving paperclip files in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,16 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
+config.paperclip_defaults = {
+  storage: :s3,
+  s3_credentials: {
+    bucket: ENV.fetch("S3_BUCKET_NAME"),
+    access_key_id: ENV.fetch("AWS_ACCESS_KEY_ID"),
+    secret_access_key: ENV.fetch("AWS_SECRET_ACCESS_KEY"),
+    Paperclip::Attachment.default_options[:url] = ":s3_domain_url",
+    Paperclip::Attachment.default_options[:path] = "/:class/:attachment/:id_partition/:style/:filename"
+  }
+}
   # Code is not reloaded between requests.
   config.cache_classes = true
 
