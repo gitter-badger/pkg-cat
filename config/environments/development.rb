e9b96d6d9591cbeada0bd05272b17289fe7d4640
George Mayorga <georgetmayorga@gmail.com>
2014-11-16 11:48:16 -0500

    Dev env doesn't send email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: '127.0.0.1', port: 3000 }
 
